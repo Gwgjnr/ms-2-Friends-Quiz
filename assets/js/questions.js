6994093bcd6cc9b6dbedf68ec946cf473a5ba188
Gwgjnr <geoffwgg@gmail.com>
2021-06-27 18:43:29 +0000

    added questions js file and inital structure to array

---
+++ assets/js/questions.js
@@ -0,0 +1,18 @@
+let questionsArray = [
+    {
+        question: "What is Joeys favourite food?",
+        optionA: "Meatball Sub",
+        optionB: "Pepperoni pizza",
+        optionC: "Roast duck",
+        optionD: "Jam",
+        answer: "A"
+    },
+    {
+        question: "What is Chandlers middle name?",
+        optionA: "Richard",
+        optionB: "Rose",
+        optionC: "Martin",
+        optionD: "Muriel",
+        answer: "D"
+    },
+]
