38bf375274093f907ced06d9dd45b85ab6bf2e11
Gwgjnr <geoffwgg@gmail.com>
2021-06-27 19:10:43 +0000

    added game js file and initial consts

---
+++ assets/js/game.js
@@ -0,0 +1,5 @@
+const question = document.getElementById("question");
+const optionA = document.getElementById("optionA");
+const optionB = document.getElementById("optionB");
+const optionC = document.getElementById("optionC");
+const optionD = document.getElementById("optionD");
