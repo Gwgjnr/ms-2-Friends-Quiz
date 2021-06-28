7a9f340dea7cacebb52da7923225cb9283a5084b
Gwgjnr <geoffwgg@gmail.com>
2021-06-28 15:07:03 +0000

    added inital structure to game.js and created random question selector function

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,3 +4,25 @@ const optionB = document.getElementById("optionB");
 const optionC = document.getElementById("optionC");
 const optionD = document.getElementById("optionD");
 
+function pickQuestion(){
+    const randomQuestion = Math.floor(Math.random() * questionsArray.length);
+    currentQ = questionsArray[randomQuestion];
+    loadQuestion(currentAskQuestion);
+
+    // still need to remove the q when selected.
+}
+
+function loadQuestion(){
+// use inner.html to display
+}
+
+function checkOption(){
+// check if correct option is selected
+// plus points if correct
+}
+
+function showHighScore(){
+// Store in local and display using inner.html
+// will need to remove lowest score
+}
+
