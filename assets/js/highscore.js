aa5ac28e3a0fdca816e14ecf921c03b2569a5aee
Gwgjnr <geoffwgg@gmail.com>
2021-07-06 00:03:28 +0000

    added further comments to code

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,14 +1,12 @@
+// Code was created using the James Quick tutorial linked in the README.md
+
 const highScoreSpan = document.getElementById("highScoreSpan");
  
 const highScore = JSON.parse(localStorage.getItem("highScore")) || [];
  
 function saveHighScore() {
     highScore.push(score_tracker);
-    console.log(score_tracker)
-    console.log(highScore)
     highScore.sort((a, b) => b - a);
-    console.log(highScore)
     highScore.splice(1);
-    console.log(highScore)
     localStorage.setItem('highScore', JSON.stringify(highScore));
 }
